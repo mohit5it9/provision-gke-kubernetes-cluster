1d8a5d3768a2243a1c16a454f234b75bccd62691
Mohit <mohit@shippable.com>
2017-10-30 12:51:44 +0530

    Update provision_gke_cluster.sh

--- provision_gke_cluster.sh
+++ provision_gke_cluster.sh
@@ -1,4 +1,4 @@
 # This script provisions a GKE cluster
 #! /bin/bash -e
 
-gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1 --zone=$2
+gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1
